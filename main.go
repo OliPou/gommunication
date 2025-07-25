1df86f480d42c6318e56bd9657a2e3c278b41f27
Adrien POPLIN <a.poplin@orisha.com>
2025-07-25 13:10:04 +0200

    chore: Remove unused host declaration from API documentation

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8081
 // @BasePath /
 // @securityDefinitions.apikey ApiKeyAuth
 // @in header
